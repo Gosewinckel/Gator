226efc32ac7d74f7f10431adb15d82e1fe438123
Lewis Gosewinckel <gosewinckel.lewis@gmail.com>
2026-05-29 10:53:10 +1000

    completed aggregator, posts are not stored in database

--- handler_agg.go
+++ handler_agg.go
@@ -3,15 +3,50 @@ package main
 import (
 	"fmt"
 	"context"
+	"time"
+
+	"github.com/Gosewinckel/Gator/internal/database"
 )
 
 func handlerAggregator(s *state, cmd command) error {
+	if len(cmd.Args) != 1 {
+		err := fmt.Errorf("Wrong numer of arguments")
+		return err
+	}
+
+	interval, err := time.ParseDuration(cmd.Args[0])
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Collecting feed every", interval)
+	ticker := time.NewTicker(interval)
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
+}
+
+func scrapeFeeds(s *state) error {
 	ctx := context.Background()
-	feed, err := fetchFeed(ctx, "https://www.wagslane.dev/index.xml")
+	nextFeed, err := s.db.GetNextFeedToFetch(ctx)
 	if err != nil {
 		return err
 	}
 	
-	fmt.Println(feed)
+	current := time.Now()
+	err = s.db.MarkFeedFetched(ctx, database.MarkFeedFetchedParams{nextFeed.ID, current})	
+	if err != nil {
+		return err
+	}
+
+	feed, err := fetchFeed(ctx, nextFeed.Url)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(feed.Channel.Title)
+	for i := range(feed.Channel.Item) {
+		fmt.Println(feed.Channel.Item[i].Title)
+	}
 	return nil
 }
